0f376dbf06705a7d7e5dbb45a18a7cea1f647521
dsajid20 <dsajid@oakland.edu>
2024-04-02 21:29:55 -0400

    assignment3

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="counter">
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <span>{count}</span>
+            <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+    );
+};
+
+export default Counter;
